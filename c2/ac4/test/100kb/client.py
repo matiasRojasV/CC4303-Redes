9c15245b1fe36d41ed4fdb535045bfbdec45a0af
MrTico89 <matias.rojas.v@ug.uchile.cl>
2026-05-28 06:56:26 -0400

    del:prints

--- c2/ac4/test/100kb/client.py
+++ c2/ac4/test/100kb/client.py
@@ -10,11 +10,7 @@ address = (IP, PUERTO)
 
 client_socketTCP = SocketTCP()
 client_socketTCP.connect(address)
-
-# ¡APAGAR el debug para maximizar la velocidad!
-client_socketTCP.DEBUG_CC = True 
-
-
+client_socketTCP.DEBUG_CC = False
 modo = "go_back_n"
 
 # test archivo 100KB
@@ -24,10 +20,7 @@ with open(archivo_path, "rb") as archivo:
 
 print("Iniciando transmisión de 100KB...")
 
-
-tiempos_ejecucion = []
-
-# Guardamos los segmentos al inicio para saber cuántos se enviaron solo en este intento
+# Guardamos los segmentos para saber cuántos se enviaron
 segmentos_iniciales = client_socketTCP.number_of_sent_segments
 
 inicio = time.time()
@@ -35,16 +28,13 @@ client_socketTCP.send(archivo_100kb, mode=modo)
 fin = time.time()
 
 tiempo_total = fin - inicio
-tiempos_ejecucion.append(tiempo_total)
-
 segmentos_enviados = client_socketTCP.number_of_sent_segments - segmentos_iniciales
 
 print(f"tiempo={tiempo_total:.4f}s, segmentos enviados en este intento={segmentos_enviados}")
 
-# --- Guardar resultados en un archivo de texto ---
 resultados_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tiempos_transmision.txt")
 with open(resultados_path, "w") as f_out:
     f_out.write("Resultados de la transmisión de 100KB\n")
     f_out.write(f"{tiempo_total:.4f} segundos\n")
     
-print(f"\n¡Prueba finalizada! Los tiempos se han guardado en: {resultados_path}")
+print(f"\n¡Prueba finalizada! Los tiempos se han guardado en:\n {resultados_path}")
--- c2/ac4/test/100kb/server.py
+++ c2/ac4/test/100kb/server.py
@@ -16,15 +16,11 @@ modo = "go_back_n"
 # test (Prueba de 100 KB de rendimiento)
 print("Test: Esperando recibir archivo de 100KB...")
 
-# 100 KB = 100 * 1024 bytes = 102400 bytes
 archivo_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "archivo_100kb.txt")
 with open(archivo_path, "rb") as archivo:
     archivo_100kb = archivo.read()
 
-TEST_5_BYTES = len(archivo_100kb)
-buff_size_100kb = TEST_5_BYTES
-
-
+buff_size_100kb = len(archivo_100kb)
 datos_100kb = connection_socketTCP.recv(buff_size_100kb, mode=modo)
 
 print(f"Recibidos: {len(datos_100kb)} bytes.")
